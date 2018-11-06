0c3b911a9d927510f18ac91a3df7aed8e48c43a2
chen hao <haoeric@hotmail.com>
2018-11-07 00:45:42 +0800

    change batch env with iam of 1TB storage

--- batch/batch_env_queue_job.sh
+++ batch/batch_env_queue_job.sh
@@ -22,28 +22,45 @@ INSTANCEROLE=arn:aws:iam::012532289196:instance-profile/iam-ecsInstanceRole-18P3
 IAMFLEETROLE=arn:aws:iam::012532289196:role/iam-spotFleetRole-7ST64BEKXAZ
 JOBROLEARN=arn:aws:iam::012532289196:role/iam-ecsTaskRole-1BIYRLSWKSLGO
 
+## create a custom AMI with attached 1TB EBS
+## EBS mount configure
+# sudo yum -y update
+# # Check the device name
+# sudo fdisk -l
+# # Make directory to where you want to mount the volume
+# sudo mkdir /docker_scratch
+# # Create filesystem on your volume 
+# sudo mkfs.ext4 /dev/xvdb
+# # Mount volume
+# sudo mount -t ext4 /dev/xvdb /docker_scratch
+# # If you want to preserve the mount after e.g. a restart, open /etc/fstab and add the mount to it
+# ## note the following path /dev/xvdb may change case by case
+# echo "/dev/xvdb /docker_scratch auto noatime 0 0" | sudo tee -a /etc/fstab
+# # Make sure nothing is wrong with fstab by mounting all
+# mount -a
 
-##-----------------------------------------------------------------------------------------------
-# Create a Batch compute environment (spot) - common vCPU config (optimal)  
-#------------------------------------------------------------------------------------------------
 
-# # Create a batch compute environment
-# ENVNAME=haoeric-sydney-optimal-spot         # environment name  
-# EC2TYPE=SPOT                                # instance resource type
-# MAXCPU=100                                  # max vCPUs in compute environment
-# SPOTPER=70                                  # percentage of on demand
-# IMAGEID=ami-00fdfc4230351e1f0               # custom image with extra mounted EBS volumn
-# instanceTypes=optimal   
+#-----------------------------------------------------------------------------------------------
+Create a Batch compute environment (spot) - 1TB common vCPU config (optimal)  
+------------------------------------------------------------------------------------------------
 
-# aws batch create-compute-environment \
-# --compute-environment-name ${ENVNAME} \
-# --type MANAGED --state ENABLED \
-# --service-role ${SERVICEROLE} \
-# --compute-resources type=${EC2TYPE},minvCpus=0,maxvCpus=${MAXCPU},desiredvCpus=0,instanceTypes=${instanceTypes},imageId=${IMAGEID},subnets=${SUBNETS},securityGroupIds=${SECGROUPS},ec2KeyPair=${KEYNAME},instanceRole=${INSTANCEROLE},bidPercentage=${SPOTPER},spotIamFleetRole=${IAMFLEETROLE}
+# Create a batch compute environment
+ENVNAME=haoeric-sydney-1TB-optimal-spot     # environment name  
+EC2TYPE=SPOT                                # instance resource type
+MAXCPU=100                                  # max vCPUs in compute environment
+SPOTPER=70                                  # percentage of on demand
+IMAGEID=ami-0c9f509ab0b5ed5ec               # custom image with extra mounted EBS volumn
+instanceTypes=optimal   
 
-# # Create the job queues
-# aws batch create-job-queue --job-queue-name highPriority-${ENVNAME} \
-# --compute-environment-order order=0,computeEnvironment=${ENVNAME} --priority 1 --state ENABLED
+aws batch create-compute-environment \
+--compute-environment-name ${ENVNAME} \
+--type MANAGED --state ENABLED \
+--service-role ${SERVICEROLE} \
+--compute-resources type=${EC2TYPE},minvCpus=0,maxvCpus=${MAXCPU},desiredvCpus=0,instanceTypes=${instanceTypes},imageId=${IMAGEID},subnets=${SUBNETS},securityGroupIds=${SECGROUPS},ec2KeyPair=${KEYNAME},instanceRole=${INSTANCEROLE},bidPercentage=${SPOTPER},spotIamFleetRole=${IAMFLEETROLE}
+
+# Create the job queues
+aws batch create-job-queue --job-queue-name highPriority-${ENVNAME} \
+--compute-environment-order order=0,computeEnvironment=${ENVNAME} --priority 1 --state ENABLED
 
 
 
--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,2 @@
-## test docker, generate results locally
-ddocker run -it -v=/Users/chenhao/GitProject/free_audio_books/data:/scratch --entrypoint /bin/bash audio_books_crawler
+## test in docker image
+docker run -it -v=/Users/chenhao/GitProject/free_audio_books/data:/scratch --entrypoint /bin/bash audio_books_crawler
